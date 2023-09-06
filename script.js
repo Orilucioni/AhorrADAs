c2ca221cbfa5a4895bae186fa54d5b3cf1164c94
Orilucioni <ori.lucioni@hotmail.com>
2023-09-06 19:09:35 -0300

    Agrego modificaciones

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const $ = (selector) => document.getElementById(selector);
 
 // Realizamos la creacion de las categorias
+const randomID = () => self.crypto.randomUUID()
 
-let categoryList = []
+let categoryList = [  
+    {
+        "nombre":"Servicio",
+        "id": randomID()
+    },
+    {
+        "nombre":"Salidas",
+        "id": randomID()
+    },
+    {
+        "nombre":"Educacion",
+        "id": randomID()
+    },
+    {   
+        "nombre":"Transporte",
+        "id": randomID()
+    },
+    {
+        "nombre":"Trabajo",
+        "id": randomID()
+    }
+     ]
 
-const createArray = () => {
-    let newItem = $("inputName").value
+
+const createArray = (lista) => {
+    let newItem = {
+        "nombre": $("inputName").value ,
+        "id": randomID()
+    }
     categoryList.push(newItem)
     createList(categoryList)
     // $("inputName").reset()
@@ -18,38 +44,39 @@ const createArray = () => {
 // })
 console.log(categoryList)
 
-const createList = (lista) => {
-    $("categoryUl").innerHTML = ""
-     lista.forEach ((item)=> {
-         let liContent = document.createTextNode(`${item}`)
-         let liItem = document.createElement("li")
-         let deleteButton = document.createElement("button")
-         deleteButton.addEventListener("click", () => deleteItem(item))
-        //  let editButton = document.createElement("button")
-        //  editButton.addEventListener("click", () => editItem(item))
-        //  liItem.classList.add ("list-item")
-         liItem.appendChild(liContent)
-         liItem.appendChild(deleteButton)
-        //  liItem.appendChild(editButton)
-         $("categoryUl").appendChild(liItem)   
-     });
-     
- }
+ const createList = (categoryList) => {
+     $("categoryUl").innerHTML = ""
+     categoryList.forEach((item) => {
+         console.log(item)
+//             let liContent = document.createTextNode(`${item.index}`)
+//             let liItem = document.createElement("li")
+// //         //    let deleteButton = document.createElement("button")
+// //         //    deleteButton.addEventListener("click", () => deleteItem(item))
+// //         //    let editButton = document.createElement("button")
+// //          //  editButton.addEventListener("click", () => editItem(item))
+//            liItem.classList.add("list-item")
+//             liItem.appendChild(liContent)
+// //         //    liItem.appendChild(deleteButton)
+// //         //     liItem.appendChild(editButton)
+//             $("categoryUl").appendChild(liItem)   
+      
+   });
+   }
 
 $ ("addButton").addEventListener("click", createArray)
-$ ("addButton").addEventListener("click", createList)
+ $ ("addButton").addEventListener("click", createList)
 
-// Seccion categorias boton eliminar
-const deleteItem = (item) => {
-const itemIndex = categoryList.indexOf(item)
-categoryList.splice(itemIndex,1)
-createList(categoryList)
-}
-
-$("deleteButton").addEventListener("click",deleteItem)
-
-// Seccion categorias boton editar
-
-// const editButton = (item) => {
-//     let nuevoValor= 
+// // Seccion categorias boton eliminar
+// const deleteItem = (item) => {
+// const itemIndex = categoryList.indexOf(item)
+// categoryList.splice(itemIndex,1)
+// createList(categoryList)
 // }
+
+// $("deleteButton").addEventListener("click",deleteItem)
+
+// // Seccion categorias boton editar
+
+// // const editButton = (item) => {
+// //     let nuevoValor= 
+// // }
