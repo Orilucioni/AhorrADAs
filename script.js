a84026879f4c97a13fc251fa22642d660e17e699
Orilucioni <ori.lucioni@hotmail.com>
2023-09-07 23:22:10 -0300

    Agrego modificaciones a la funciones de categorias

--- script.js
+++ script.js
@@ -59,66 +59,88 @@ const randomID = () => self.crypto.randomUUID()
 
 let categoryList = [  
     {
-        "nombre":"Servicio",
-        "id": randomID()
+        nombre:"Servicio",
+        id: randomID()
     },
     {
-        "nombre":"Salidas",
-        "id": randomID()
+        nombre:"Salidas",
+        id: randomID()
     },
     {
-        "nombre":"Educacion",
-        "id": randomID()
+        nombre:"Educacion",
+        id: randomID()
     },
     {   
-        "nombre":"Transporte",
-        "id": randomID()
+        nombre:"Transporte",
+        id: randomID()
     },
     {
-        "nombre":"Trabajo",
-        "id": randomID()
-    }
+        nombre:"Trabajo",
+        id: randomID()
+    },
      ]
 
-
 const createArray = (lista) => {
-    let newItem = {
-        "nombre": $("inputName").value ,
-        "id": randomID()
-    }
-    categoryList.push(newItem)
+    // let newItem ={
+    //     "nombre": $("inputName").value ,
+    //     "id": randomID()
+    // }
+    // categoryList.push(newItem)
     createList(categoryList)
-    // $("inputName").reset()
 }
 
-// $ ("inputName").addEventListener("keydown", function(event){
-//     if (event.key === "Enter") {
-//         createArray()
-//     }
-// })
 console.log(categoryList)
 
+// const createItems = (categoryList) => {
+//     categoryList.forEach((items) => {
+//         console.log(items)
+//         $("categoryUl").innerHTML = ""
+//         $("categoryUl").innerHTML +=`<li>Servicio</li> 
+//         <div>
+//           <button class="button">Editar</button> <button class="button">Eliminar</button>
+//         </div> 
+//         <li>Salidas</li> 
+//         <div>
+//           <button class="button">Editar</button> <button class="button">Eliminar</button>
+//         </div> 
+//         <li>Educacion</li> 
+//         <div>
+//           <button class="button">Editar</button> <button class="button">Eliminar</button>
+//         </div> 
+//         <li>Transporte</li> 
+//         <div>
+//           <button class="button">Editar</button> <button class="button">Eliminar</button>
+//         </div> 
+//         <li>Trabajo</li> 
+//         <div>
+//           <button class="button">Editar</button> <button class="button">Eliminar</button>
+//         </div> `
+//     })
+// }
+// createItems(categoryList)
+
+
  const createList = (categoryList) => {
-     $("categoryUl").innerHTML = ""
-     categoryList.forEach((item) => {
-         console.log(item)
-//             let liContent = document.createTextNode(`${item.index}`)
-//             let liItem = document.createElement("li")
+      $("categoryUl").innerHTML = ""
+      categoryList.forEach((item) => {
+          console.log(item)
+             let liContent = document.createTextNode(`${item.index}`)
+             let liItem = document.createElement("li")
 // //         //    let deleteButton = document.createElement("button")
 // //         //    deleteButton.addEventListener("click", () => deleteItem(item))
 // //         //    let editButton = document.createElement("button")
 // //          //  editButton.addEventListener("click", () => editItem(item))
-//            liItem.classList.add("list-item")
-//             liItem.appendChild(liContent)
+            liItem.classList.add("list-item")
+             liItem.appendChild(liContent)
 // //         //    liItem.appendChild(deleteButton)
 // //         //     liItem.appendChild(editButton)
-//             $("categoryUl").appendChild(liItem)   
+             $("categoryUl").appendChild(liItem)   
       
-   });
-   }
+    });
+    }
 
 $ ("addButton").addEventListener("click", createArray)
- $ ("addButton").addEventListener("click", createList)
+  $ ("addButton").addEventListener("click", createList)
 
 // // Seccion categorias boton eliminar
 // const deleteItem = (item) => {
